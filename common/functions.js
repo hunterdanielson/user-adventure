fe3ff612c846fd22411c75fd2a719e083e1486f9
Hunter Danielson <hunterleedanielson@gmail.com>
2020-04-15 22:53:04 -0700

    added all pages

---
+++ common/functions.js
@@ -0,0 +1,52 @@
+import { getUser } from '../data/user-api.js';
+
+// to load the stuff in the header
+export function loadProfile() {
+    const name = document.getElementById('name');
+    const avatar = document.getElementById('avatar');
+    const hp = document.getElementById('hp');
+    const gold = document.getElementById('gold');
+
+    // get the user data
+    const user = getUser();
+    
+    name.textContent = user.name;
+    avatar.src = '../assets/user/' + user.character + '.png';
+    
+    gold.textContent = user.gold;
+
+    // check if user is dead, if are show they died, if not show their hp
+    if (isDead(user)) {
+        hp.textContent = 'Dead';
+    } else {
+        hp.textContent = user.hp;
+    }
+}
+
+export function isDead(user) {
+    return user.hp <= 0;
+} 
+
+export function findById(passedarray, id) {
+    for (let i = 0; i < passedarray.length; i++) {
+        const item = passedarray[i];
+        // check the id against item.id
+        if (item.id === id) {
+            return item;
+        }
+    }
+    return null;
+}
+
+export function completedAllDungeons(dungeons, user) {
+    // loop through each dungeon
+    dungeons.forEach(dungeon => {
+        // check if its dungeon id appears on the user completed object, if not user not done
+        if (!user.comleted[dungeon.id]) {
+            return false;
+        }
+    });
+    // if it makes it here all dungeons have passed
+    return;
+
+}
--- map/map.js
+++ map/map.js
@@ -1,2 +1,30 @@
 import { getUser } from '../data/user-api.js';
-import dungeons from '../data/dungeons.js';
+import { dungeons } from '../data/dungeons.js';
+import { loadProfile, isDead, completedDungeons } from '../common/functions.js';
+import { renderDungeonLinks, renderCompletedDungeon } from './renders.js';
+
+loadProfile();
+
+// grab the user from storage
+const user = getUser();
+
+// check if the game is over
+if (isDead(user) || completedDungeons(dungeons, user)) {
+    window.location = '../results';
+}
+
+const dungeonsDiv = document.getElementById('dungeons');
+
+dungeons.forEach(dungeon => {
+    let dungeonDisplay = null;
+    if (user.completed[dungeon.id]) {
+        // if completed just show text
+        dungeonDisplay = renderCompletedDungeon(dungeon);
+    } else {
+        // if not complete make the link for each dungeon
+        dungeonDisplay = renderDungeonLinks(dungeon);
+    }
+    // push each onto the map
+    dungeonsDiv.appendChild(dungeonDisplay);
+
+});
